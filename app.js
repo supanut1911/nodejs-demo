d499eaad2498a6ae773450fe55b924116e51ee89
Supanut1911 <ynotmela@gmail.com>
2024-02-08 14:58:21 +0700

    add api lists

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get("/health", (req, res) => {
   res.json({ status: "healthy" });
 });
 
+app.get("/lists", (req, res) => {
+  res.json([
+    { id: 1, name: "macbook" },
+    { id: 2, name: "iphone" },
+    { id: 3, name: "airpod" },
+  ]);
+});
+
 app.listen(4000, () => {
   console.log("app is running");
 });
